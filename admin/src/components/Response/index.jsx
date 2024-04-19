7df1c56353165fe6fc873c3fb7b54326b335643b
theCompanyDream <tbrantleyii@gmail.com>
2024-04-19 19:01:59 -0400

    forgot this

--- admin/src/components/Response/index.jsx
+++ admin/src/components/Response/index.jsx
@@ -1,38 +1,20 @@
-import React from "react";
-
+import React from 'react';
 import { Box, Typography } from "@strapi/design-system";
+import DOMPurify from 'dompurify';
 
-const style = {
-  wordBreak: "keep-all",
-};
 
-function transfromBotText(message) {
-    // If Url return render Img
-  if (Array.isArray(message)){
-    return (<Typography style={style}>ChatGPT: <a href={message[1]}>Picture Link</a>
-    <br />
-    <img src={message[0]} height={300} width={350} />
-    </Typography>)
-  }
+const Response = ({ children }) => {
+    const cleanHtml = DOMPurify.sanitize(children.message);
+    const htmlContent = {
+        __html: cleanHtml
+    };
 
-  return (<Typography style={style}>ChatGPT: {message}</Typography>)
-
-}
-
-const Response = ({ data }) => {
-  if (!data || !data.you || !data.bot) return null;
-
-  return (
-    <Box>
-      <span>
-        <Typography style={style}>You: {data.you}</Typography>
-      </span>
-      <br />
-      <span>
-        <br/>{transfromBotText(data.bot)}
-      </span>
-    </Box>
-  );
+    return (
+        <Box>
+            {/* Safely render HTML content */}
+            {children.name}: <span dangerouslySetInnerHTML={htmlContent} />
+        </Box>
+    );
 };
 
 export default Response;
